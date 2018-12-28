ce9890893b47a61b5414c027c6d82ec09565f3f8
yasen <jbashov@gmail.com>
2018-12-28 10:37:36 +0200

    Added TemplateMethod

---
+++ ConsoleApplication1/Behavioral.TemplateMethod/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Behavioral.TemplateMethod
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            DataAccessObject daoCategories = new Categories();
+            daoCategories.Run();
+
+            DataAccessObject daoProducts = new Products();
+            daoProducts.Run();
+
+            // Wait for user
+
+            Console.ReadKey();
+        }
+    }
+}
